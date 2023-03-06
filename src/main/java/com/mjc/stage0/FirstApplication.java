66f23490d48d99d93a63a5118c99617b0aa69ace
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-06 22:52:16 +0300

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -10,12 +10,13 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class FirstApplication {
     public static void main(String[] args) throws IOException {
         String fileName = "src/main/java/talks/mjc/FirstApplication.java";
-        String searc = "Write your code here!";
-        String replac = "Hello, World!";
+         String searc = "Write your code here!";
+        String replac = "Hello, World! System.out.println";
         Charset charset = UTF_8;
         Path path = Paths.get(fileName);
         String content = new String(Files.readAllBytes(path), charset);
         content = content.replaceAll(searc, replac);
         Files.write(path, content.getBytes(charset));
+        System.out.println(content);
     }
 }
