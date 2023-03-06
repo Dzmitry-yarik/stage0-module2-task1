d1d6b88f989d5b78ce78c53d1671c5147fdc4bbe
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-06 22:46:33 +0300

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,7 +1,21 @@
 package com.mjc.stage0;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class FirstApplication {
-    public static void main(String[] args) {
-        // Write your code here!
+    public static void main(String[] args) throws IOException {
+        String fileName = "src/main/java/talks/mjc/FirstApplication.java";
+        String searc = "Write your code here!";
+        String replac = "Hello, World!";
+        Charset charset = UTF_8;
+        Path path = Paths.get(fileName);
+        String content = new String(Files.readAllBytes(path), charset);
+        content = content.replaceAll(searc, replac);
+        Files.write(path, content.getBytes(charset));
     }
 }
